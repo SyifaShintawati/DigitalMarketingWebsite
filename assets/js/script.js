b77700f31e09a761366b40e0e3c1ab49cc757591
LAPTOP-244V2VPB\ASUS <gesitnurpratama80@gmail.com>
2023-11-21 22:19:36 +0700

    gesit : add content for section about us and services

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+// Slider Services
+let serviceCard = document.querySelector(
+  "#service-slider"
+);
+if (window.matchMedia("(min-width: 768px)").matches) {
+  let carousel = new bootstrap.Carousel(serviceCard, {
+    interval: false,
+  });
+  let serviceWidth = $(".carousel-inner")[0].scrollWidth;
+  let cardWidth = $(".carousel-item").width();
+  let scrollPosition = 0;
+  $("#service-slider .carousel-control-next").on("click", function () {
+    if (scrollPosition < serviceWidth - cardWidth * 4) {
+      scrollPosition += (cardWidth + 50);
+      $("#service-slider .carousel-inner").animate(
+        { scrollLeft: scrollPosition },
+        600
+      );
+    }
+  });
+  $("#service-slider .carousel-control-prev").on("click", function () {
+    if (scrollPosition > 0) {
+      scrollPosition -= (cardWidth + 50);
+      $("#service-slider .carousel-inner").animate(
+        { scrollLeft: scrollPosition },
+        600
+      );
+    }
+  });
+} else {
+  $(serviceCard).addClass("slide");
+}
